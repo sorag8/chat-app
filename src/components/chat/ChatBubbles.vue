913a07697e2986b8e2a88c432b7386cbbfb6bdda
Sora <armand.g8@gmail.com>
2024-09-11 17:08:01 -0600

    Separación en componentes

---
+++ src/components/chat/ChatBubbles.vue
@@ -0,0 +1,16 @@
+<template>
+  <!-- Messages go here -->
+  <!-- Example Message -->
+  <div class="flex justify-end">
+    <div class="bg-blue-200 text-black p-2 rounded-lg max-w-xs">Hey, how's your day going?</div>
+  </div>
+
+  <!-- Example Received Message -->
+  <div class="flex">
+    <div class="bg-gray-300 text-black p-2 rounded-lg max-w-xs">
+      Not too bad, just a bit busy. How about you?
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
---
+++ src/components/chat/ChatMessages.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex-1 overflow-y-auto p-4">
+    <div class="flex flex-col space-y-2">
+      <ChatBubbles />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import ChatBubbles from './ChatBubbles.vue';
+</script>
